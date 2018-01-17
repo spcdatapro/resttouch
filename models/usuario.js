980261f347e5742da3f87ce56b1ebbcb24092357
JAragon <jaragon@spcdatapro.com>
2018-01-17 13:10:39 -0600

    Cambios al 17012018 1310

---
+++ models/usuario.js
@@ -0,0 +1,15 @@
+'use strict'
+
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var UserSchema = Schema({
+    nombre: String,
+    usuario: {type: String, unique: true},
+    contrasenia: String,
+    correoe: {type: String, unique: true},
+    roles: [],
+    debaja: Boolean    
+});
+
+module.exports = mongoose.model('usuario', UserSchema, 'usuario');
