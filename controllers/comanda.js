30342a5f2e7b54b446948f5c328259cec473195c
JAragon <jaragon@spcdatapro.com>
2018-01-31 21:45:47 -0600

    Agregados cantidad de lineas de detalle en el API de restaurantes; API para avisar que hay error en la recepción en restaurante; detalle de los estatus de comanda con fecha y hora; cambio de tiempo transcurrido a la fecha/hora en que terminan de tomar el pedido. 31/01/2018 21:45

--- controllers/comanda.js
+++ controllers/comanda.js
@@ -1,5 +1,6 @@
 'use strict'
 
+var mongoose = require('mongoose');
 var moment = require('moment');
 
 // Modelos
@@ -37,7 +38,8 @@ function crearComanda(req, res){
     com.idrestaurante = params.idrestaurante;
     com.idmotorista = params.idmotorista;
     com.imgpago = params.imgpago;
-    com.debaja = params.debaja;    
+    com.debaja = params.debaja;   
+    com.bitacoraestatus = [ { idestatuscomanda: mongoose.Types.ObjectId("59fea7304218672b285ab0e2"), estatus: "Pendiente", fecha: moment().toDate() } ];
 
     com.save((err, comandaSvd) => {
         if (err) {
@@ -463,6 +465,22 @@ function traduceDetalleComanda(dc, dict) {
     return detalle;
 }
 
+function conteoProductos(det) {
+    var conteo = 0;
+
+    if (det) {
+        det.forEach(function(item){
+            try{
+                if (item.idproducto.trim() != '') {
+                    conteo++;
+                }
+            }catch(e){ }
+        });
+    }
+
+    return conteo;
+}
+
 function traduceFormasPago(dp, emisores) {
     let detalle = [], str = '';
     dp.forEach((fp) => {
@@ -504,7 +522,7 @@ function listaComandasRestaurante(req, res) {
                     res.status(200).send({ mensaje: 'No se encontro nada en el diccionario de fox.' });
                 } else {
                     const diccionario = dictfox;
-                    Comanda.find({ idestatuscomanda: "59fea7304218672b285ab0e2", debaja: false }, null, { sort: { fecha: 1 } })
+                    Comanda.find({ idestatuscomanda: { $in: ["59fea7304218672b285ab0e2", "5a72403fc5bb328b700edc58"] }, debaja: false }, null, { sort: { fecha: 1 } })
                         .populate('idcliente', ['_id', 'nombre'])
                         .populate('idtipocomanda', ['_id', 'descripcion', 'imagen'])
                         .populate('idusuario', ['_id', 'nombre'])
@@ -536,7 +554,11 @@ function listaComandasRestaurante(req, res) {
                                                 };
                                             }
 
+                                            var tmpDetalle = [], cantDetalle = 0;
+
                                             if (rst.idrestaurante._id.toString().trim() === idrestaurante.toString().trim()) {
+                                                tmpDetalle = traduceDetalleComanda(rst.detallecomanda, diccionario);
+                                                cantDetalle = conteoProductos(tmpDetalle);
                                                 lst.push({
                                                     fechahora: moment(rst.fecha, 'YYYY-MM-DD HH:mm:ss').format('DD/MM/YYYY HH:mm:ss'),
                                                     tracking: parseInt(rst.tracking),
@@ -554,9 +576,11 @@ function listaComandasRestaurante(req, res) {
                                                     restaurante: rst.idrestaurante ? rst.idrestaurante.nombre : rst.iddireccioncliente.idrestaurante.nombre,
                                                     idtipocomanda: rst.idtipocomanda._id,
                                                     tipocomanda: rst.idtipocomanda.descripcion,
-                                                    detalle: traduceDetalleComanda(rst.detallecomanda, diccionario),
+                                                    detalle: tmpDetalle,
+                                                    cantidadlineasdetalle: cantDetalle,
                                                     detalleformapago: traduceFormasPago(rst.detcobrocomanda, listaet),
-                                                    facturara: traduceFacturarA(rst.detfacturara[0])
+                                                    facturara: traduceFacturarA(rst.detfacturara[0]),
+                                                    bitacoraestatus: rst.bitacoraestatus
                                                 });
                                             }
                                         } catch (errTry) {
@@ -617,6 +641,10 @@ function getComandaByTracking(req, res) {
                                                 };
                                             }
 
+                                            var tmpDetalle = [], cantDetalle = 0;
+                                            tmpDetalle = traduceDetalleComanda(rst.detallecomanda, diccionario);
+                                            cantDetalle = conteoProductos(tmpDetalle);
+
                                             lst.push({
                                                 fechahora: moment(rst.fecha, 'YYYY-MM-DD HH:mm:ss').format('DD/MM/YYYY HH:mm:ss'),
                                                 tracking: parseInt(rst.tracking),
@@ -634,9 +662,11 @@ function getComandaByTracking(req, res) {
                                                 restaurante: rst.idrestaurante ? rst.idrestaurante.nombre : rst.iddireccioncliente.idrestaurante.nombre,
                                                 idtipocomanda: rst.idtipocomanda._id,
                                                 tipocomanda: rst.idtipocomanda.descripcion,
-                                                detalle: traduceDetalleComanda(rst.detallecomanda, diccionario),
+                                                detalle: tmpDetalle,
+                                                cantidadlineasdetalle: cantDetalle,
                                                 detalleformapago: traduceFormasPago(rst.detcobrocomanda, listaet),
-                                                facturara: traduceFacturarA(rst.detfacturara[0])
+                                                facturara: traduceFacturarA(rst.detfacturara[0]),
+                                                bitacoraestatus: rst.bitacoraestatus
                                             });                                            
                                         } catch (errTry) {
                                             errores.push(errTry);
@@ -657,12 +687,57 @@ function getComandaByTracking(req, res) {
 
 }
 
+function setBody(idestatus){
+    var descripcion = '';
+
+    switch (idestatus) {
+        case "59fea7304218672b285ab0e2": descripcion = "Pendiente"; break;
+        case "59fea7524218672b285ab0e3": descripcion = "Recibido en restaurante"; break;
+        case "5a512f55228d627facf8c643": descripcion = "Confirmado por encargado"; break;
+        case "59fea7894218672b285ab0e4": descripcion = "Cobrado - aprobado"; break;
+        case "59fea79e4218672b285ab0e5": descripcion = "Cobrado - rechazado"; break;
+        case "59fea7bc4218672b285ab0e6": descripcion = "Producción"; break;
+        case "59fea7dd4218672b285ab0e7": descripcion = "En camino"; break;
+        case "59fea7f34218672b285ab0e8": descripcion = "Entregado"; break;
+        case "5a72403fc5bb328b700edc58": descripcion = "Error al recibir en restaurante"; break;
+    }
+
+    return {
+        idestatuscomanda: idestatus,
+        $push: {
+            'bitacoraestatus': {
+                fecha: moment().toDate(),
+                idestatuscomanda: mongoose.Types.ObjectId(idestatus),
+                estatus: descripcion
+            }
+        }
+    };
+}
+
+function comandaConProblemas(req, res){
+    var idcom = req.params.id;
+    var body = setBody("5a72403fc5bb328b700edc58");
+
+    Comanda.findByIdAndUpdate(idcom, body, { new: true }, (err, comandaUpd) => {
+        if (err) {
+            res.status(500).send({ mensaje: 'Error en el servidor al poner la comanda en Problemas.' });
+        } else {
+            if (!comandaUpd) {
+                res.status(200).send({ mensaje: 'No se pudo poner con problema para descargar la comanda.' });
+            } else {
+                res.status(200).send({ mensaje: 'Comanda puesta con problemas para descargar.', entidad: comandaUpd });
+            }
+        }
+    });
+}
+
 function confirmarComanda(req, res){
     var idcom = req.params.id;
+    var body = setBody("59fea7524218672b285ab0e3");
 
-    Comanda.findByIdAndUpdate(idcom, { idestatuscomanda: "59fea7524218672b285ab0e3"}, { new: true }, (err, comandaUpd) => {
+    Comanda.findByIdAndUpdate(idcom, body, { new: true }, (err, comandaUpd) => {
         if (err) {
-            res.status(500).send({ mensaje: 'Error en el servidor al confirmar la comanda.' });
+            res.status(500).send({ mensaje: 'Error en el servidor al confirmar la comanda. ERROR: ' + err });
         } else {
             if (!comandaUpd) {
                 res.status(200).send({ mensaje: 'No se pudo confirmar la comanda.' });
@@ -675,8 +750,9 @@ function confirmarComanda(req, res){
 
 function confirmarComandaEncargado(req, res) {
     var idcom = req.params.id;
+    var body = setBody("5a512f55228d627facf8c643");
 
-    Comanda.findByIdAndUpdate(idcom, { idestatuscomanda: "5a512f55228d627facf8c643" }, { new: true }, (err, comandaUpd) => {
+    Comanda.findByIdAndUpdate(idcom, body, { new: true }, (err, comandaUpd) => {
         if (err) {
             res.status(500).send({
                 mensaje: 'Error en el servidor al confirmar la comanda por el encargado.'
@@ -708,8 +784,9 @@ function resetEstatusComandas(req, res) {
 
 function cobroAprobadoComanda(req, res) {
     var idcom = req.params.id;
+    var body = setBody("59fea7894218672b285ab0e4");
 
-    Comanda.findByIdAndUpdate(idcom, { idestatuscomanda: "59fea7894218672b285ab0e4" }, { new: true }, (err, comandaUpd) => {
+    Comanda.findByIdAndUpdate(idcom, body, { new: true }, (err, comandaUpd) => {
         if (err) {
             res.status(500).send({ mensaje: 'Error en el servidor al cambiar el estatus a cobro aprobado de la comanda.' });
         } else {
@@ -724,8 +801,9 @@ function cobroAprobadoComanda(req, res) {
 
 function cobroRechazadoComanda(req, res) {
     var idcom = req.params.id;
+    var body = setBody("59fea79e4218672b285ab0e5");
 
-    Comanda.findByIdAndUpdate(idcom, { idestatuscomanda: "59fea79e4218672b285ab0e5" }, { new: true }, (err, comandaUpd) => {
+    Comanda.findByIdAndUpdate(idcom, body, { new: true }, (err, comandaUpd) => {
         if (err) {
             res.status(500).send({ mensaje: 'Error en el servidor al cambiar el estatus a cobro rechazado de la comanda.' });
         } else {
@@ -740,8 +818,9 @@ function cobroRechazadoComanda(req, res) {
 
 function produccionComanda(req, res) {
     var idcom = req.params.id;
+    var body = setBody("59fea7bc4218672b285ab0e6");
 
-    Comanda.findByIdAndUpdate(idcom, { idestatuscomanda: "59fea7bc4218672b285ab0e6" }, { new: true }, (err, comandaUpd) => {
+    Comanda.findByIdAndUpdate(idcom, body, { new: true }, (err, comandaUpd) => {
         if (err) {
             res.status(500).send({ mensaje: 'Error en el servidor al cambiar el estatus a produción de la comanda.' });
         } else {
@@ -756,8 +835,10 @@ function produccionComanda(req, res) {
 
 function enCaminoComanda(req, res) {
     var idcom = req.params.id, idmotorista = req.params.idmotorista;
+    var body = setBody("59fea7dd4218672b285ab0e7");
+    body.idmotorista = idmotorista;
 
-    Comanda.findByIdAndUpdate(idcom, { idestatuscomanda: "59fea7dd4218672b285ab0e7", idmotorista: idmotorista }, { new: true }, (err, comandaUpd) => {
+    Comanda.findByIdAndUpdate(idcom, body, { new: true }, (err, comandaUpd) => {
         if (err) {
             res.status(500).send({ mensaje: 'Error en el servidor al cambiar el estatus a en camino de la comanda.' });
         } else {
@@ -772,8 +853,9 @@ function enCaminoComanda(req, res) {
 
 function entregadaComanda(req, res) {
     var idcom = req.params.id;
+    var body = setBody("59fea7f34218672b285ab0e8");
 
-    Comanda.findByIdAndUpdate(idcom, { idestatuscomanda: "59fea7f34218672b285ab0e8" }, { new: true }, (err, comandaUpd) => {
+    Comanda.findByIdAndUpdate(idcom, body, { new: true }, (err, comandaUpd) => {
         if (err) {
             res.status(500).send({ mensaje: 'Error en el servidor al cambiar el estatus a comanda entregada.' });
         } else {
@@ -1190,7 +1272,7 @@ module.exports = {
     crearComanda, modificarComanda, eliminarComanda, listaComandas, getComanda, lstComandasCliente, contadorPorEstatus, lstComandasUsuario, listaComandasPost,
     // api para FOX
     listaComandasRestaurante, confirmarComanda, resetEstatusComandas, cobroAprobadoComanda, cobroRechazadoComanda,
-    produccionComanda, enCaminoComanda, entregadaComanda, confirmarComandaEncargado, getComandaByTracking,
+    produccionComanda, enCaminoComanda, entregadaComanda, confirmarComandaEncargado, getComandaByTracking, comandaConProblemas,
     // fin de api para FOX    
     crearDetComanda, modificarDetComanda, eliminarDetComanda, listaDetComanda, getDetComanda,
     crearDetCompDetComanda, modificarDetCompDetComanda, eliminarDetCompDetComanda, listaDetCompDetComanda, getDetCompDetComanda,
